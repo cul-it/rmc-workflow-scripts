20410cbe1eac1f449891359e1c012abc62f598fd
Dianne Dietrich <dd388@cornell.edu>
2016-07-07 16:56:37 -0400

    Stub file

---
+++ disk-image-triage/level1-data.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Input: Directory of directories of E01/info pairs (or triples, etc.)
+# Output: CSV file with aggregated metadata
+#         bulk_extractor output in appropriate directory, when produced.
+# Error: Input directory in wrong format
