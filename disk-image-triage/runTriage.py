087c1a6342881f58a1b63294ffbfd13bc879bf00
Dianne Dietrich <dd388@cornell.edu>
2017-08-11 12:06:22 -0400

    runTriage with subscripts

---
+++ disk-image-triage/runTriage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Input:
+# Output:
+# Error:
+
+
+import sys
+import os
+import argparse
+import re
+import csv
+from shutil import move
+from glob import glob
+from hurry.filesize import size
+
+import organizeDirs
+import guymagerLogMD
+import filesystemID
+import level1Data
+import mergeOutputs
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputdir', metavar='[input_dir]',
+                        help='input directory with E01/info files')
+    args = parser.parse_args()
+
+    try:
+        os.path.exists(args.inputdir)
+    except:
+        sys.exit('Quitting: Input directory does not exist.')
+
+    # Check for space; quit if not available
+    input_size = 0
+    for dirpath, dirnames, filenames in os.walk(args.inputdir):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            input_size += os.path.getsize(fp)
+    print("The size of the input directory is: {0}".format(size(input_size)))
+    print("To continue, you need {0} of free space.".format(size(input_size*4)))
+    proceed = input("Do you want to continue? y/N ")
+    if proceed.lower != 'y':
+        sys.exit("Quitting.")
+
+    # Run all the individual triage scripts
+    organizeDirs.main(args.inputdir)
+    guymagerLogMD.main(args.inputdir)
+    filesystemID.main(args.inputdir)
+    level1Data.main(args.inputdir)
+    mergeOutputs.main(args.inputdir)
+
+if __name__ == "__main__":
+    main()
